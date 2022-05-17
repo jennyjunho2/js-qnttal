c0b9527503d954e2145089508e8836605ddad3ba
jennyjunho2 <a01032208149@gmail.com>
2022-05-17 21:51:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-qnttal
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qnttal)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+const dan = {
+  type: 'person',
+  data: {
+    gender: 'male',
+    info: {
+      id: 22,
+      fullname: {
+        first: 'Dan',
+        last: 'Deacon',
+      },
+    },
+  },
+};
+
+const deepPick = (fields, object = {}) => {
+  const [first, ...remaining] = fields.split('.');
+  return remaining.length
+    ? deepPick(remaining.join('.'), object[first])
+    : object[first];
+};
+
+console.log(deepPick('type', dan));
+console.log(deepPick('data.info.fullname.first', dan));
