51f5a328f0cf810ef761510226089d6d5fd9c2b8
jennyjunho2 <a01032208149@gmail.com>
2022-05-17 21:58:45 +0900

    1 file modified

--- index.js
+++ index.js
@@ -3,26 +3,39 @@ import './style.css';
 
 // Write Javascript code!
 
-const dan = {
-  type: 'person',
-  data: {
-    gender: 'male',
-    info: {
-      id: 22,
-      fullname: {
-        first: 'Dan',
-        last: 'Deacon',
-      },
-    },
-  },
-};
+const oneSecond = () => 1000;
+const getCurrentTime = () => new Date();
+const clear = () => console.clear();
+const log = (msg) => console.log(msg);
 
-const deepPick = (fields, object = {}) => {
-  const [first, ...remaining] = fields.split('.');
-  return remaining.length
-    ? deepPick(remaining.join('.'), object[first])
-    : object[first];
-};
+const abstractClockTime = (date) => ({
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  seconds: date.getSeconds(),
+});
 
-console.log(deepPick('type', dan));
-console.log(deepPick('data.info.fullname.first', dan));
+const civilianHours = (clockTime) => ({
+  ...clockTime,
+  hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours,
+});
+
+const appendAMPM = (clockTime) => ({
+  ...clockTime,
+  ampm: clockTime.hours >= 12 ? 'PM' : 'AM',
+});
+
+const display = (target) => (time) => target(time);
+
+const formatClock = (format) => (time) =>
+  format
+    .replace('hh', time.hours)
+    .replace('mm', time.minutes)
+    .replace('ss', time.seconds)
+    .reaplce('tt', time.ampm);
+
+const prependZero = (key) => (clockTime) => ({
+  ...clockTime,
+  [key]: clockTime[key] < 10 ? '0' + clockTime[key] : clockTime[key],
+});
+
+const convertToCivilianTime = (clockTime) => compose();
